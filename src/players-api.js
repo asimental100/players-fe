d3f573ad009a1daabbc4abfbb385e56c22e2a596
Adrian Simental <asimental100@hotmail.com>
2020-08-12 19:28:00 -0700

    smaller changes

--- src/players-api.js
+++ src/players-api.js
@@ -6,9 +6,8 @@ const URL = 'https://guarded-beach-96626.herokuapp.com/';
 
 export function fetchPlayers() {
     return request.get(`${URL}/players`);
-    }
+}
 
-    export function fetchPlayer(id) {
+export function fetchPlayer(id) {
     return request.get(`${URL}/players/${id}`);
-    }
-  
+}
